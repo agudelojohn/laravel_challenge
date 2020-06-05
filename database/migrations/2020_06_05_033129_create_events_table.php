0b6fbb6fd885b93a690229d54dcb0fa23b31415f
agudelojohn <jhbr08@hotmail.com>
2020-06-04 22:56:52 -0500

    Models
    Models had been created

---
+++ database/migrations/2020_06_05_033129_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->char('title',50);
+            $table->longText('description');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->char('status',1);
+            $table->unsignedBigInteger('userOwner');
+            $table->foreign('userOwner')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
